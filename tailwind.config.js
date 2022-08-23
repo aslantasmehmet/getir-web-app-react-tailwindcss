26eb38acf3cbfff37afaa230133ead7f7184bf42
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-08-23 23:29:07 +0300

    HeroSection component was designed and coded.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    backgroundColor: theme => ({
-      'brand-color': '#5d3ebc'
-    }),
-    extend: {},
+    extend: {
+      colors: {
+        'brand-color': '#4c3398',
+        'primary-brand-color': '#5d3ebc',
+        'secondary-brand-color': '#7849f7',
+        'brand-yellow' : '#ffd300'
+      }
+    },
   },
   plugins: [],
 }
