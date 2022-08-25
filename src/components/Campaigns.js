5d8e763bd623b90dd5a4cc50ae1ddf6370e1f2da
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-08-25 11:32:26 +0300

    Photos of the campaign were brought from the API. Required css operations applied.

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -28,39 +28,19 @@ export default function Campaigns() {
 
     return (
         <div>
+            <div className='container mx-auto py-8'>
+                <h3 className='font-semibold text-sm mb-2'>Kampanyalar</h3>
+                <Slider {...settings} className="container -mx-2">
+                    {banners.length && banners.map((banner, index) => (
+                        <div key={banner.id}>
+                            <picture className='block px-2'>
+                                <img src={banner.image} className='rounded-lg' />
+                            </picture>
 
-            <Slider {...settings} className="container mx-auto">
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div>
-                <div>
-                    <h3>7</h3>
-                </div>
-                <div>
-                    <h3>8</h3>
-                </div>
-                <div>
-                    <h3>9</h3>
-                </div>
-            </Slider>
-
-
-
+                        </div>
+                    ))}
+                </Slider>
+            </div>
         </div >
     )
 }
