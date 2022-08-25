91ebf5f36b40467f4819fca5da4b9996139692ea
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-08-25 09:45:02 +0300

    The categories component was designed. According to the request from the API, the listing process has been done successfully.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
+import Categories from './components/Categories';
 
 function App() {
   return (
     <div >
       <Header/>
       <HeroSection/>
+      <Categories/>
     </div>
   );
 }
