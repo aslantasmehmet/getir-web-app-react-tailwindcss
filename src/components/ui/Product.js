0813bd2ecea55ac8273e3ddd79130d0d1f3e3488
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-08-26 11:47:05 +0300

    edit was made.

--- src/components/ui/Product.js
+++ src/components/ui/Product.js
@@ -6,7 +6,7 @@ export default function Product({ product }) {
     <div className='bg-white gap-y-1 relative block flex flex-col items-center text-center p-3 '>
       <picture >
         <button>
-          <AiOutlinePlus className='absolute top-3 right-3 w-5 h-5 bg-white flex items-center justify-center border border-gray-200 rounnded-lg twxt-white shadow-md hover:bg-brand-color hover:text-brand-yellow hover:border-brand-color' />
+          <AiOutlinePlus className=' absolute top-3 right-3 w-5 h-5 bg-white flex items-center justify-center border border-gray-200 rounded-lg twxt-white shadow-md hover:bg-brand-color hover:text-brand-yellow hover:border-brand-color' />
         </button>
       </picture>
 
